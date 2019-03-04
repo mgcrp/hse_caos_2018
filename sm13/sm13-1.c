166792573fa8f9e3f4c3de39214d44336e3afb75
Ilya Popov <edu@popov.tech>
2019-03-04 22:36:47 +0300

    Update sm13-1.c

--- sm13/sm13-1.c
+++ sm13/sm13-1.c
@@ -1,22 +1,32 @@
-#include <stdio.h>
-#include <time.h>
 #include <inttypes.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+enum {
+    // 1 day == 24hours * 60minutes * 60seconds
+    DAY_IN_SECONDS = 86400,
+    // buffer size
+    BUF_SIZE = 20
+};
 
 int main() {
-    int32_t ttime;
-    time_t unixt;
-
-    while (scanf("%" SCNd32, &ttime) == 1) {
-        time_t cur = time(NULL);
-        if (__builtin_mul_overflow(ttime, 24*3600, &unixt) || __builtin_add_overflow(cur, unixt, &cur)) {
+    int32_t days_in;
+    time_t temp_delta_time;
+    time_t temp_final_time;
+    while (scanf("%" SCNd32, &days_in) == 1) {
+        time_t current_time = time(NULL);
+        if (__builtin_mul_overflow(days_in, DAY_IN_SECONDS, &temp_delta_time)) {
             printf("OVERFLOW\n");
             continue;
         }
-        char buf[20];
-        strftime(buf, 20, "%F", localtime(&cur));
+        if (__builtin_add_overflow(current_time, temp_delta_time, &temp_final_time)) {
+            printf("OVERFLOW\n");
+            continue;
+        }
+        char buf[BUF_SIZE];
+        strftime(buf, BUF_SIZE, "%F", localtime(&temp_final_time));
         printf("%s\n", buf);
     }
-
     return 0;
 }
