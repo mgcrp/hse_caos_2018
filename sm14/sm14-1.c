cc253f54eee999c8eaec0b4be4ea08d11d93e969
Ilya Popov <edu@popov.tech>
2019-03-05 18:46:23 +0300

    Update sm14-1.c

--- sm14/sm14-1.c
+++ sm14/sm14-1.c
@@ -7,49 +7,49 @@
 #include <unistd.h>
 
 int main(int argc, char ** argv) {
-	long long int answer = 0;
+    long long int answer = 0;
 
-	DIR * current_dir = opendir(argv[1]);
-	// Ошибка при загрузке директории
-	if (!current_dir) {
-		printf("0\n");
-		return 0;
-	}
+    DIR * current_dir = opendir(argv[1]);
+    // Ошибка при загрузке директории
+    if (!current_dir) {
+        printf("0\n");
+        return 0;
+    }
 
-	// Получение UID текущего пользователя
-	uid_t current_user = getuid();
-	// Чтение файлов внутри папки
-	struct dirent * file;
-	while ((file = readdir(current_dir))) {
-		char * file_name = file->d_name;
+    // Получение UID текущего пользователя
+    uid_t current_user = getuid();
+    // Чтение файлов внутри папки
+    struct dirent * file;
+    while ((file = readdir(current_dir))) {
+        char * file_name = file->d_name;
 
-		char file_path[PATH_MAX];
-		snprintf(file_path, PATH_MAX, "%s/%s", argv[1], file_name);
+        char file_path[PATH_MAX];
+        snprintf(file_path, PATH_MAX, "%s/%s", argv[1], file_name);
 
-		struct stat file_stats;
-		int stat_ret = stat(file_path, &file_stats);
+        struct stat file_stats;
+        int stat_ret = stat(file_path, &file_stats);
 
-		// Ошибка при чтении stats
-		if (stat_ret) {
-			continue;
-		}
-		// Файл регулярный
-		if (!S_ISREG(file_stats.st_mode)) {
-			continue;
-		}
-		// Его владельцем является текущий пользователь
-		if (current_user != file_stats.st_uid) {
-			continue;
-		}
-		// Его имя начинается с заглавной латинской буквы
-		if (!isupper(file_name[0])) {
-			continue;
-		}
-		// Если все ок - размер файла можно прибавить к ответу
-		answer += file_stats.st_size;
-	}
+        // Ошибка при чтении stats
+        if (stat_ret) {
+            continue;
+        }
+        // Файл регулярный
+        if (!S_ISREG(file_stats.st_mode)) {
+            continue;
+        }
+        // Его владельцем является текущий пользователь
+        if (current_user != file_stats.st_uid) {
+            continue;
+        }
+        // Его имя начинается с заглавной латинской буквы
+        if (!isupper(file_name[0])) {
+            continue;
+        }
+        // Если все ок - размер файла можно прибавить к ответу
+        answer += file_stats.st_size;
+    }
 
-	closedir(current_dir);
-	printf("%lld\n", answer);
-	return 0;
+    closedir(current_dir);
+    printf("%lld\n", answer);
+    return 0;
 }
