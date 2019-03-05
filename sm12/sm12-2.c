b7768433310f791188d80f66732b898c5ef1e16b
Ilya Popov <edu@popov.tech>
2019-03-05 13:45:54 +0300

    Update sm12-2.c
    Updated after reject

--- sm12/sm12-2.c
+++ sm12/sm12-2.c
@@ -5,6 +5,9 @@ void copy_file(int in_fd, int out_fd) {
     ssize_t r_ret;
     while ((r_ret = read(in_fd, buf, 4096)) > 0) {
         ssize_t w_ret = write(out_fd, buf, r_ret);
+        if (w_ret == -1) {
+            exit 1;
+        }
         while (w_ret < r_ret) {
             w_ret += write(out_fd, buf + w_ret, r_ret - w_ret);
         }
