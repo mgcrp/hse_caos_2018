3f9ce1321b79101b1cea53390e39afa7cb7f65e0
Ilya Popov <edu@popov.tech>
2019-03-05 14:01:47 +0300

    Update sm12-2.c
    Updated after compilation error

--- sm12/sm12-2.c
+++ sm12/sm12-2.c
@@ -6,7 +6,7 @@ void copy_file(int in_fd, int out_fd) {
     while ((r_ret = read(in_fd, buf, 4096)) > 0) {
         ssize_t w_ret = write(out_fd, buf, r_ret);
         if (w_ret == -1) {
-            exit 1;
+            exit(1);
         }
         while (w_ret < r_ret) {
             w_ret += write(out_fd, buf + w_ret, r_ret - w_ret);
