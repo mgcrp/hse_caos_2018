1a649ddb3de82e4d40d182bde50da527b5978f22
Ilya Popov <edu@popov.tech>
2019-03-11 12:44:36 +0300

    Update sm14-2.c

--- sm14/sm14-2.c
+++ sm14/sm14-2.c
@@ -2,23 +2,18 @@
 #include <stdio.h>
 
 int main(int argc, char ** argv) {
-    if (argc < 2) {
-        return 0;
-    }
+    if (argc < 2) { return 0; }
+
     char * functionName = argv[1];
     void * handle = dlopen("/lib/libm.so.6", RTLD_LAZY);
-    if (handle == NULL) {
-        return 1;
-    }
-
+    if (handle == NULL) { return 1; }
     void * function = dlsym(handle, functionName);
-    if (function == NULL) {
-        return 1;
-    }
+    if (function == NULL) { return 1; }
 
-    double number;
-    while ((scanf("%lf", &number)) == 1) {
-        double result = ((double(*)(double)) function)(number);
+    double number_in;
+    while ( (scanf("%lf", &number_in)) == 1 ) {
+        // complicated stuff from seminar
+        double result = ((double(*)(double)) function)(number_in);
         printf("%.10g\n", result);
     }
 
