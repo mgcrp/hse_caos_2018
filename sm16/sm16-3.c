65f2fe052474136fd509c7f6e3e97083c5a1f46b
Ilya Popov <edu@popov.tech>
2019-04-04 13:36:12 +0300

    Update sm16-3.c

--- sm16/sm16-3.c
+++ sm16/sm16-3.c
@@ -8,9 +8,7 @@ int main() {
     int N;
     scanf("%d", N);
     for (int i = 0; i < N; ++i) {
-        if (pid == 0) {
-            printf("%d" , i + 1);
-        }
+        printf("%d " , i + 1);
         if (i == N - 1) {
             exit(0);
         }
