160fc9956f4a549dc76aab0e1538784402d85819
Ilya Popov <edu@popov.tech>
2019-04-04 14:09:55 +0300

    Create sm16-2_ver2.c

---
+++ sm16/sm16-2_ver2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void child(int N, int index) {
+    printf(" %d", index);
+    if (index < N) {
+        pid_t pid = fork();
+        if (pid < 0) {
+            exit(1);
+        }
+        if (pid == 0) {
+            child(N, index + 1);
+        }
+    }
+
+    wait(NULL);
+    exit(0);
+}
+
+int main() {
+    int N, index = 1;
+    scanf("%d", &N);
+    printf("%d", index);
+    pid_t pid = fork();
+    if (pid < 0) {
+        exit(1);
+    }
+    if (pid == 0) {
+        child(N, index + 1);
+    }
+
+    wait(NULL);
+    printf("\n");
+}
