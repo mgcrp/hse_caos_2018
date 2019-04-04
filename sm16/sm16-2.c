596707a8c09cf1eadffbd011fee81bad7b9174c3
Ilya Popov <edu@popov.tech>
2019-04-04 13:02:21 +0300

    Update sm16-2.c

--- sm16/sm16-2.c
+++ sm16/sm16-2.c
@@ -1,24 +1,28 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <inttypes.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 void child(int index) {
+    // Get string
     char BUF[8];
     ssize_t result = read(0, BUF, 8);
+    // Exit if error accured while reading
     if (result != 8) {
         exit(1);
     }
+    // String to Int
     int32_t num = strtol(BUF, NULL, 10);
+    // Output
     printf("%d %"PRId32"\n", index, num * num);
     exit(0);
 }
 
 int main() {
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 1; i <= 3; ++i) {
         if (!fork()) {
-            child(i + 1);
+            child(i);
         }
     }
 
