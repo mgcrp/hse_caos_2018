4dca21725032447932491608a589c92c7a9feaef
Ilya Popov <edu@popov.tech>
2019-04-04 13:26:18 +0300

    Create sm16-3.c

---
+++ sm16/sm16-3.c
@@ -0,0 +1,21 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+    int N;
+    scanf("%d", N);
+    for (int i = 0; i < N; ++i) {
+        printf("%d", i + 1);
+        if (i == N - 1) {
+            exit(0);
+        }
+        pid_t pid = fork();
+        if (pid != 0) {
+            break;
+        }
+    }
+    wait(NULL);
+}
