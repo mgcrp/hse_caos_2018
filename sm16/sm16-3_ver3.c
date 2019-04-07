23423c8abf7c2634a56dcc8232fa2da041acf016
Ilya Popov <edu@popov.tech>
2019-04-07 16:20:15 +0300

    Update sm16-3_ver3.c

--- sm16/sm16-3_ver3.c
+++ sm16/sm16-3_ver3.c
@@ -9,24 +9,21 @@ int main() {
     scanf("%d", &N);
 
     for (int i = 0; i < N; ++i) {
-        if (i == N - 1) {
+        if (i == N - 1)
             printf("%d\n", i + 1);
-        } else {
+        else
             printf("%d ", i + 1);
-        }
 
         fflush(stdout);
 
-        if (i == N - 1) {
+        if (i == N - 1)
             exit(0);
-        }
 
         pid_t pid = fork();
-        if (pid < 0) {
+        if (pid < 0)
             exit(1);
-        } else if (pid != 0) {
+        else if (pid != 0)
             break;
-        }
     }
     wait(NULL);
 }
