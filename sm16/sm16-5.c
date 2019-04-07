be2241894efaa1790ad5bbd82615a92ddb07edf2
Ilya Popov <edu@popov.tech>
2019-04-08 00:05:00 +0300

    Ошибка синхронизации

--- sm16/sm16-5.c
+++ sm16/sm16-5.c
@@ -10,19 +10,15 @@ void writer(char filename[], int N, int A, int D, int K, int process_number) {
     if (pid < 0) {
         exit(1);
     } else if (pid == 0) {
-        printf("Hello, I'm process #%d\n", process_number);
-        printf("process %d k = %d\n", process_number, K);
-
         int fd = open(filename, O_RDWR, 0666);
         int start_number = A + (process_number * D);
         for (int j = 0; j < K; ++j) {
-            printf("process %d k = %d\n", process_number, j);
             int off = lseek(fd, (N * j * 4) + (process_number * 4), SEEK_SET);
             if (off < 0) {
                 exit(1);
             }
-            int current_num = start_number + (process_number * K * j);
-            printf("process %d print %d pos %d\n", process_number, current_num, (N * j * 4) + (process_number * 4));
+            int current_num = start_number + (N * j * D);
+            // printf("process %d print %d pos %d\n", process_number, current_num, (N * j * 4) + (process_number * 4));
             int res = write(fd, &current_num, 4);
             if (res != 4) {
                 exit(1);
