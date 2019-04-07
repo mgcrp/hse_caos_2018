d5598b85db6044b5165d7ddd113f70a32bdbb73e
Ilya Popov <edu@popov.tech>
2019-04-07 19:53:24 +0300

    Create sm16-4.c

---
+++ sm16/sm16-4.c
@@ -0,0 +1,39 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// TODO:
+//  b = запоминаем пид главного процесса
+//  while (считываение):
+//      с = форк
+//      if (с > 0):
+//          d = waitpid
+//          if (d == 0):
+//              print(число)
+//          else if (d == 1):
+//          if (getpid() == b):
+//              break;
+//          плохой Exit
+//
+
+int main() {
+    int number;
+    pid_t root_pid = getpid();
+    while (scanf("%d", &number) != EOF) {
+        pid_t fork_ret = fork();
+        if (fork_ret > 0) {
+            pid_t waitpid_ret = waitpid(fork_ret);
+            if (waitpid_ret == 0) {
+                printf("%d\n", number);
+                exit(0);
+            } else if (getpid() == root_pid) {
+                break;
+            } else {
+                exit(1);
+            }
+        }
+    }
+    return 0;
+}
