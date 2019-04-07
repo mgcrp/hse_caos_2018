09e518c9147ec06d02c7abcbb162486111f2e1c6
Ilya Popov <edu@popov.tech>
2019-04-07 18:29:08 +0300

    Update sm16-3.c

--- sm16/sm16-3.c
+++ sm16/sm16-3.c
@@ -6,16 +6,36 @@
 
 int main() {
     int N;
-    scanf("%d", N);
+    scanf("%d", &N);
+    pid_t pid_start = getpid();
+
     for (int i = 0; i < N; ++i) {
-        printf("%d " , i + 1);
+        // Выводим то, что от нас просят
+        if (i == N - 1) {
+            printf("%d\n" , i + 1);
+        } else {
+            printf("%d ", i + 1);
+        }
+
+        // Все остальное уходит в буффер
+        fflush(stdout);
+
+        // Если напечатано последнее число - выход
         if (i == N - 1) {
             exit(0);
         }
+        // Иначе - генерим новый процесс
         pid_t pid = fork();
+        // Если возникла ошибка - выходим с кодом 1
+        if (pid < 0) {
+            exit(1);
+        }
+        // Прерываем цикл для процесса-родителя
         if (pid != 0) {
             break;
         }
     }
+
     wait(NULL);
+    return 0;
 }
