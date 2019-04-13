e230e65c60d5adb73ff2bf7e036a8a6ae3ea52ac
Ilya Popov <edu@popov.tech>
2019-04-13 11:45:45 +0300

    Create kr06-2.c

---
+++ kr06/kr06-2.c
@@ -0,0 +1,30 @@
+#include <dlfcn.h>
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+    if (argc < 4) {
+        return 0
+    };
+
+    double number;
+    sscanf(argv[1], "%f", &number);
+    char prevFile[];
+
+    int current_arg = 2;
+    while (current_arg <= argc - 1) {
+        char * functionName = argv[current_arg + 1];
+        if (argv[current_arg] == '-') {
+            void * handle = dlopen(prevFile, RTLD_LAZY);
+        } else {
+            void * handle = dlopen(argv[current_arg], RTLD_LAZY);
+        }
+        if (handle == NULL) { continue; }
+        prevFile = argv[current_arg];
+        void * function = dlsym(handle, functionName);
+        if (function == NULL) { continue; }
+        number = ((double(*)(double)) function)(number);
+    }
+
+    printf("%.6g\n", number);
+    return 0;
+}
