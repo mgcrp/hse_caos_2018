4d8566687cbd9efe7775698f91f7f8f629670dc3
Ilya Popov <edu@popov.tech>
2019-04-15 11:54:14 +0300

    Update sm17-1.c

--- sm17/sm17-1.c
+++ sm17/sm17-1.c
@@ -7,12 +7,15 @@ int main(int argc, char * argv[]) {
     pid_t pid = fork();
 
     if (pid == 0) {
-        int fdInput = open(argv[2], O_RDONLY);
-        dup2(fd, STDIN_FILENO);
+        // Открытие входного файла
+        int file_in = open(argv[2], O_RDONLY);
+        dup2(file_in, STDIN_FILENO);
 
-        int fdOutput = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-        dup2(fdOutput, STDIN_FILENO);
+        // Открытие выходного файла
+        int file_out = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        dup2(file_out, STDOUT_FILENO);
 
+        // Выполнение команды
         execlp(argv[1], argv[1], NULL);
         exit(1);
     }
