fd7c8e1392aab03c26b7dcccbe1e4fec21115885
Ilya Popov <edu@popov.tech>
2019-04-15 12:50:04 +0300

    Update sm17-3.c

--- sm17/sm17-3.c
+++ sm17/sm17-3.c
@@ -1,23 +1,33 @@
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-int exec_logical(char * cmd) {
+int exec(char * str) {
+    // Функция запуска и проверки условия ¯\_(ツ)_/¯
     pid_t pid = fork();
+
     if (pid < 0) {
+        // Ошибка запуска процесса - return 0;
         return 0;
     } else if (pid == 0) {
-        execlp(cmd, cmd, NULL);
+        // Всё ок - запускаем команду в процессе-ребенке
+        execlp(str, str, NULL);
         exit(1);
     }
-
-    int status;
-    wait(&status);
-    if (WIFEXITED(status)) {
-        return (WEXITSTATUS(status) == 0);
+    int wait_ret;
+    wait(&wait_ret);
+    if (WIFEXITED(wait_ret)) {
+        // Делаем логический вывод:
+        //     [0 - в shell всё ок] -> True в С;
+        //     [!0 - в shell не ок] -> False в С;
+        return (WEXITSTATUS(wait_ret) == 0);
     }
 }
 
 int main(int argc, char ** argv) {
-    return !( (exec_logical(argv[1]) || exec_logical(argv[2]) ) && exec_logical(argv[3]) );
+    // Выполняем (<логический вывод 1> или <логический вывод 2>) и <логический вывод 3>
+    // Так как вывод должен быть в shell-логике:
+    //     !True - !0 as Bytes - 1 as C output;
+    //     !False - !1 as Bytes - 0 as C output;
+    return !( (exec(argv[1]) || exec(argv[2]) ) && exec(argv[3]) );
 }
