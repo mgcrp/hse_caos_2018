dc291d0eb12ff5d6405b296c0e7f26bb5e5a3fa6
Ilya Popov <edu@popov.tech>
2019-04-15 12:29:34 +0300

    Update sm17-2.c

--- sm17/sm17-2.c
+++ sm17/sm17-2.c
@@ -6,24 +6,34 @@
 
 enum {
     SIG_EXIT_CONST = 128,
-    CANNOT_EXEC_SHELL = 127
+    BIN_SH_FAILED = 127
 };
 
 int mysys(const char * str) {
+    // Создание процесса
     int pid = fork();
+
     if (pid < 0) {
+        // Ошибка создания процесса - return -1;
         return -1;
     } else if (pid == 0) {
         execl("/bin/sh", "sh", "-c", str, NULL);
-        _exit(CANNOT_EXEC_SHELL);
+        // Интерпретатор `/bin/sh` не может быть запущен -
+        // функция завершается со значением 127
+        _exit(BIN_SH_FAILED);
     }
 
-    int status;
-    waitpid(pid, &status, 0);
-    if (WIFEXITED(status)) {
-        return WEXITSTATUS(status);
-    } else if (WIFSIGNALED(status)) {
-        return SIG_EXIT_CONST + WTERMSIG(status);
+    // Ожидание завершения процесса
+    int waitpid_ret;
+    waitpid(pid, &waitpid_ret, 0);
+
+    if (WIFEXITED(waitpid_ret)) {
+        // Завершение с кодом 0-127 - return <код>;
+        return WEXITSTATUS(waitpid_ret);
+    } else if (WIFSIGNALED(waitpid_ret)) {
+        // Завершение из-за сигнала - return 128 + <сигнал>
+        return SIG_EXIT_CONST + WTERMSIG(waitpid_ret);
     }
+    
     return 0;
 }
