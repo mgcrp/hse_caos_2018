5f7d7ecb02629b5c5e80cfd2259e82e364b120d8
Ilya Popov <edu@popov.tech>
2019-04-22 17:49:24 +0300

    Update sm18-1.c

--- sm18/sm18-1.c
+++ sm18/sm18-1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main(int argc, char * argv[]) {
     if (argc < 3) {
@@ -9,18 +9,18 @@ int main(int argc, char * argv[]) {
     }
 
     int fd[2];
-    int pid1, pid2;
     if (pipe(fd) < 0) {
         perror("pipe");
         exit(1);
     }
 
-    if ((pid1 = fork()) < 0) {
+    int pid1 = fork();
+    if (pid1 < 0) {
         perror("fork");
         exit(1);
     }
     if (!pid1) {
-        /* child 1: "ls -l" */
+        // Child #1 - executing first CMD from argv[1]
         dup2(fd[1], 1);
         close(fd[0]);
         close(fd[1]);
@@ -29,12 +29,13 @@ int main(int argc, char * argv[]) {
         _exit(1);
     }
 
-    if ((pid2 = fork()) < 0) {
+    int pid2 = fork()
+    if (pid2 < 0) {
         perror("fork");
         exit(1);
     }
     if (!pid2) {
-        /* child 2: "wc -l" */
+        // Child #2 - executing second CMD from argv[2]
         dup2(fd[0], 0);
         close(fd[0]);
         close(fd[1]);
