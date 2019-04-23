bae051899684830b8590eca68ccd6bde32139f27
Ilya Popov <edu@popov.tech>
2019-04-23 13:17:53 +0300

    kr06-2 passed Ejudge

--- kr06/kr06-2.c
+++ kr06/kr06-2.c
@@ -1,30 +1,53 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char * argv[]) {
-    if (argc < 4) {
+    // argv[0] - имя файла
+    // argv[1] - float x0
+    // argv[2] - первая библиотека -> (2 * i) - i-ая библиотека
+    // argc[3] - первая функция -> (2 * i + 1) - i-ая функция
+    if (argc == 2) {
+        // Подали только число - выводим его в требуемом формате
+        float number = strtof(argv[1], NULL);
+        printf("%.6g\n", number);
+        return 0;
+    } else if (argc < 4) {
+        // Ошибка
         return 0;
     }
 
-    double number;
-    sscanf(argv[1], "%f", &number);
-    char prevFile[] = "";
-
+    // Считываю первый float
+    float number = strtof(argv[1], NULL);
+    // Индекс предыдущей библиотеки
+    int prev_lib = 2;
+    // Индекс текущей библиотеки
     int current_arg = 2;
+
     while (current_arg <= argc - 1) {
-        char * functionName = argv[current_arg + 1];
         void * handle;
-        if (argv[current_arg] == "-") {
-            void * handle = dlopen(prevFile, RTLD_LAZY);
+        if (strcmp(argv[current_arg], "-")) {
+            // Не равно "-"
+            handle = dlopen(argv[current_arg], RTLD_LAZY);
+            prev_lib = current_arg;
         } else {
-            void * handle = dlopen(argv[current_arg], RTLD_LAZY);
+            // равно "-"
+            handle = dlopen(argv[prev_lib], RTLD_LAZY);
         }
-        if (handle == NULL) { continue; }
-        strpcy(prevFile, argv[current_arg])
-        void * function = dlsym(handle, functionName);
-        if (function == NULL) { continue; }
-        number = ((double(*)(double)) function)(number);
+        if (handle == NULL) {
+            current_arg += 2;
+            continue;
+        }
+        void * function = dlsym(handle, argv[current_arg + 1]);
+        if (function == NULL) {
+            current_arg += 2;
+            continue;
+        }
+        number = ((float(*)(float)) function)(number);
+        if (dlclose(handle) < 0) { exit(1); }
+
+        current_arg += 2;
     }
 
     printf("%.6g\n", number);
--- kr06/kr06-2_ver2.c
+++ kr06/kr06-2_ver2.c
@@ -1,52 +1,30 @@
 #include <dlfcn.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char * argv[]) {
-    // argv[0] - имя файла
-    // argv[1] - float x0
-    // argv[2] - первая библиотека -> (2 * i) - i-ая библиотека
-    // argc[3] - первая функция -> (2 * i + 1) - i-ая функция
-    if (argc == 2) {
-        // Подали только число - выводим его в требуемом формате
-        double number;
-        sscanf(argv[1], "%lg", &number);
-        printf("%.6g\n", number);
-        return 0;
-    } else if (argc < 4) {
-        // Ошибка
+    if (argc < 4) {
         return 0;
     }
 
-    // Считываю первый float
     double number;
-    sscanf(argv[1], "%lg", &number);
-    // Индекс предыдущей библиотеки
-    int prev_lib = 2;
-    // Индекс текущей библиотеки
-    int current_arg = 2;
+    sscanf(argv[1], "%f", &number);
+    char prevFile[] = "";
 
+    int current_arg = 2;
     while (current_arg <= argc - 1) {
         char * functionName = argv[current_arg + 1];
         void * handle;
-
-        if (strcmp(argv[current_arg], "-")) {
-            // Не равно "-"
-            handle = dlopen(argv[current_arg], RTLD_LAZY);
-            prev_lib = current_arg;
+        if (argv[current_arg] == "-") {
+            void * handle = dlopen(prevFile, RTLD_LAZY);
         } else {
-            // равно "-"
-            handle = dlopen(argv[prev_lib], RTLD_LAZY);
+            void * handle = dlopen(argv[current_arg], RTLD_LAZY);
         }
-
         if (handle == NULL) { continue; }
+        strpcy(prevFile, argv[current_arg])
         void * function = dlsym(handle, functionName);
         if (function == NULL) { continue; }
         number = ((double(*)(double)) function)(number);
-        if (dlclose(handle) < 0) { exit(1); }
-
-        current_arg += 2;
     }
 
     printf("%.6g\n", number);
