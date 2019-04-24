bd11830ac70b074af4ecb4b42444b708622a3d86
Ilya Popov <edu@popov.tech>
2019-04-24 16:45:53 +0300

    Create sm20-1.c

---
+++ sm20/sm20-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main() {
+    struct addrinfo * temp;
+    struct addrinfo * result;
+    struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM };
+    char host_in[1000], service_in[1000];
+
+    while (scanf("%s %s", &host_in, &service_in) != EOF) {
+        int ret_code;
+
+        ret_code = getaddrinfo(host_in, service_in, &hints, &result);
+        if (ret_code) {
+            printf("%s\n", gai_strerror(ret_code));
+            continue;
+        }
+    }
+}
