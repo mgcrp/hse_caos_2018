58284c8dab41e90ff5f727329a55341c5ac19abe
Ilya Popov <edu@popov.tech>
2019-04-24 19:05:38 +0300

    Update sm20-1.c

--- sm20/sm20-1.c
+++ sm20/sm20-1.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 
 int main() {
     struct addrinfo * temp;
@@ -20,17 +21,35 @@ int main() {
             continue;
         }
 
+        char ipmin_str[INET_ADDRSTRLEN];
+        unsigned long ipmin_binary;
+        unsigned short ipmin_port;
+        bool is_first = true;
+
         for(temp = result; temp != NULL; temp = temp->ai_next) {
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)temp->ai_addr;
             void* addr = &(ipv4->sin_addr);
             inet_ntop(temp->ai_family, addr, ipstr, sizeof ipstr);
             int port = ntohs( ((struct sockaddr_in *)( (struct sockaddr *)temp->ai_addr ))->sin_port );
             // printf("  %s: %s\n", ipver, ipstr);
-            printf("%s:%d\n", ipstr, port);
+            printf("\t%s:%d\n", ipstr, port);
 
-	    unsigned long lol = ipv4->sin_addr.s_addr;
-            printf("%X\n", ntohl(lol));
+            unsigned long lol = ipv4->sin_addr.s_addr;
+            lol = ntohl(lol);
+
+            if (is_first) {
+                is_first = false;
+                strcpy(ipstr, ipmin_str);
+                ipmin_binary = lol;
+                ipmin_port = port;
+            } else if (lol < ipmin_binary) {
+                strcpy(ipstr, ipmin_str);
+                ipmin_binary = lol;
+                ipmin_port = port;
+            }
         }
+
+        printf("%s:%d\n", ipmin_str, ipmin_port);
 
         freeaddrinfo(result); // free the linked list
     }
