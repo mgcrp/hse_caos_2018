0129f4d38a4a3b77919593f3df7fce18eb91afbc
Ilya Popov <edu@popov.tech>
2019-04-24 17:17:09 +0300

    Update sm20-1.c

--- sm20/sm20-1.c
+++ sm20/sm20-1.c
@@ -24,11 +24,11 @@ int main() {
             struct sockaddr_in *ipv4 = (struct sockaddr_in *)temp->ai_addr;
             void* addr = &(ipv4->sin_addr);
             inet_ntop(temp->ai_family, addr, ipstr, sizeof ipstr);
-            int port = ntohs( ((struct sockaddr_in *)( (struct sockaddr *)temp)->ai_addr )->sin_port ));
+            int port = ntohs( ( (struct sockaddr_in *)( ((struct sockaddr *)temp)->ai_addr ) )->sin_port );
             // printf("  %s: %s\n", ipver, ipstr);
             printf("%s:%d\n", ipstr, port);
         }
 
-        freeaddrinfo(res); // free the linked list
+        freeaddrinfo(result); // free the linked list
     }
 }
