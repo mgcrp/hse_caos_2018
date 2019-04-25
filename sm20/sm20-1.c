6c11a91b75db0b5715b0d951f64abdab359819ff
Ilya Popov <edu@popov.tech>
2019-04-25 15:30:54 +0300

    Update sm20-1.c

--- sm20/sm20-1.c
+++ sm20/sm20-1.c
@@ -12,7 +12,7 @@ int main() {
     struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM };
 
     char host_in[1000], service_in[1000];
-    while (scanf("%s %s", &host_in, &service_in) != EOF) {
+    while (scanf("%s %s", host_in, service_in) != EOF) {
         int ret_code = getaddrinfo(host_in, service_in, &hints, &result);
         if (ret_code) {
             printf("%s\n", gai_strerror(ret_code));
