c41db5c30dc8aaae37995834475cd6e895527306
Ilya Popov <edu@popov.tech>
2019-04-25 16:30:14 +0300

    Update sm20-2.c

--- sm20/sm20-2.c
+++ sm20/sm20-2.c
@@ -7,6 +7,60 @@
 #include <unistd.h>
 #include <signal.h>
 
-int main() {
+int main(int argc, char * argv[]) {
+    // checking if we get exactly 3 command line arguments
+    if (argc != 4) { return 0; }
 
+    // initializing stuff
+    struct addrinfo * result;
+    struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM };
+    signal(SIGPIPE, SIG_IGN);
+
+    // getting address info
+    int ret_code = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (ret_code) { return 0; }
+
+    // setting up connection, saving it's file descriptor
+    int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) { return 0; }
+
+    // connecting
+    ret_code = connect(sfd, result->ai_addr, result->ai_addrlen);
+    if (ret_code < 0) { return 0; }
+
+    // initializing read and write streams
+    int socket_fd2 = dup(socket_fd);
+    FILE * read_stream = fdopen(socket_fd, "r");
+    FILE * write_stream = fdopen(socket_fd2, "w");
+
+    // sending word from argv[3] -> to server
+    ret_code = fprintf(write_stream, "%s\n", argv[3]);
+    fflush(write_stream);
+    if (ret_code < 0) { return 0; }
+
+    // recieving number <- from server
+    int number;
+    ret_code = fscanf(read_stream, "%d", &number);
+    if (ret_code < 0) { return 0; }
+
+    // sending numbers from 0 to {number} -> to server
+    for (int i = 0; i <= k; ++i) {
+        ret_code = fprintf(write_stream, "%d\n", i);
+        fflush(write_stream);
+        if (ret_code < 0) { return 0; }
+    }
+
+    // recieving final number <- from server
+    unsigned long long answer;
+    ret_code = fscanf(read_stream, "%llu", &answer);
+    if (ret < 0) { return 0; }
+
+    // printing final code
+    printf("%llu\n", answer);
+
+    // finishing program
+    fflush(stdout);
+    fclose(read_stream);
+    fclose(write_stream);
+    return 0;
 }
