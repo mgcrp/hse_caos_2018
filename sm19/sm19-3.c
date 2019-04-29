190983c5d40827148904b116ea0a41c2a9a12187
Ilya Popov <edu@popov.tech>
2019-04-29 11:38:51 +0300

    Update sm19-3.c

--- sm19/sm19-3.c
+++ sm19/sm19-3.c
@@ -15,37 +15,42 @@ void signal_handler(int signo) {
 }
 
 int main() {
+    // initializing signal handler
     struct sigaction action = { .sa_flags = SA_RESTART, .sa_handler = signal_handler };
     sigaction(SIGUSR1, &action, NULL);
     sigaction(SIGUSR2, &action, NULL);
     sigaction(SIGTERM, &action, NULL);
 
-    sigset_t mask, oldmask;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-    sigaddset(&mask, SIGUSR2);
-    sigaddset(&mask, SIGTERM);
+    // initializing:
+    //     action_mask - blocks singals while process is working with the prev one
+    //     wait_mask - used in sigsuspend while waiting for one of expected signals
+    sigset_t action_mask, wait_mask;
+    sigemptyset(&action_mask);
+    sigaddset(&action_mask, SIGUSR1);
+    sigaddset(&action_mask, SIGUSR2);
+    sigaddset(&action_mask, SIGTERM);
+    sigprocmask(SIG_BLOCK, &action_mask, &wait_mask);
 
-    sigprocmask(SIG_BLOCK, &mask, &oldmask);
-
+    // printing PID
     printf("%d\n", getpid());
     fflush(stdout);
 
     int cnt_sigusr1 = 0, cnt_sigusr2 = 0;
     while (42) {
         while(signal_in == 0) {
-            sigsuspend(&oldmask);
+            // waiting for the signal
+            sigsuspend(&wait_mask);
         }
         switch (signal_in) {
             case 1:
                 printf("%d %d\n", cnt_sigusr1, cnt_sigusr2);
+                fflush(stdout);
                 cnt_sigusr1 += 1;
             case 2:
                 cnt_sigusr2 += 1;
             case 3:
                 _exit(0);
         }
-        fflush(stdout);
         signal_in = 0;
     }
 }
