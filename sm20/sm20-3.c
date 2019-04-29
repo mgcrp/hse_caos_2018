a6bf744bbbdd43010a4e8b43ed10a2acc33d55ae
Ilya Popov <edu@popov.tech>
2019-04-29 13:22:36 +0300

    Update sm20-3.c

--- sm20/sm20-3.c
+++ sm20/sm20-3.c
@@ -13,18 +13,34 @@ int main(int argc, char * argv[]) {
     setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &value, sizeof(socket_fd));
     setsockopt(socket_fd, SOL_SOCKET, SO_REUSEPORT, &value, sizeof(socket_fd));
 
+    // describing address
     unsigned short port_in = strtol(argv[1], NULL, 10);
     struct sockaddr_in address = { .sin_addr.s_addr = INADDR_ANY, .sin_family = AF_INET, .sin_port = htons(port_in) };
 
+    // bind
     int ret_code;
     ret_code = bind(socket_fd, (struct sockaddr *) &address, sizeof(address));
-    if (ret_code < 0) { return 1; }
+    if (ret_code < 0) { return 0; }
 
-    ret_code = listen(lfd, 5);
-    if (ret_code < 0) { return 1; }
+    // listen
+    ret_code = listen(socket_fd, 5);
+    if (ret_code < 0) { return 0; }
 
     while (42) {
+        int size = sizeof(address);
+        int single_conn_fd = accept(socket_fd, (struct sockaddr *) &address, (socklen_t *)&size);
+        if (single_conn_fd < 0) { _exit(0); }
 
+        ret_code = read(afd, &number_in, sizeof(number_in))
+        if (ret_code < sizeof(number_in)) { _exit(0); }
+
+        if (number != 0) {
+            result += ntohl(number_in);
+        } else {
+            break;
+        }
+
+        close(single_conn_fd);
     }
 
     printf("%d\n", sum);
