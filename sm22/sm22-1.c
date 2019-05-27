bba159cecdd117adb2de5c08e75616fbf2c6499e
Ilya Popov <edu@popov.tech>
2019-05-27 22:58:37 +0300

    Create sm22-1.c

---
+++ sm22/sm22-1.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+
+enum { THREAD_NUM = 10 };
+
+struct ThreadConfig {
+    int id;
+    pthread_t prev;
+};
+
+void * thread_handler(void * params) {
+    struct ThreadConfig config = *((struct ThreadConfig *) params);
+    if (config.id > 0) {
+        pthread_join(config.prev, NULL);
+    }
+    printf("%d\n", config.id);
+    fflush(stdout);
+    return NULL;
+}
+
+int main() {
+    struct ThreadConfig config[THREAD_NUM];
+    pthread_t thread = 0;
+
+    for (int i = 0; i < THREAD_NUM; ++i) {
+        config[i].id = i;
+        config[i].prev = thread;
+        pthread_create(&thread, NULL, thread_handler, config[i]);
+    }
+
+    pthread_join(thread, NULL);
+    return 0;
+}
