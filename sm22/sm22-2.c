4837662a813647a125f6b509c1ee335b171d9a19
Ilya Popov <edu@popov.tech>
2019-05-27 21:56:30 +0300

    Update sm22-2.c

--- sm22/sm22-2.c
+++ sm22/sm22-2.c
@@ -4,7 +4,7 @@
 void * thread_handler(void * params) {
     (void)(params);
     int number_in;
-    if (scanf("%d", %number_in) == 1) {
+    if (scanf("%d", &number_in) == 1) {
         pthread_t thread;
         pthread_create(&thread, NULL, thread_handler, NULL);
         pthread_join(thread, NULL);
